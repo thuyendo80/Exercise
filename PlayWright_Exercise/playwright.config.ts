f9c0e3350165b021cb44eb63e2b088decd09ac6d
Thuyen Do <thuyen.do80@gmail.com>
2025-09-11 12:59:16 +0700

    change resolution

--- PlayWright_Exercise/playwright.config.ts
+++ PlayWright_Exercise/playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    viewport: { width: 1920, height: 1080 }, // Disable Playwright's viewport
   },
 
   /* Configure projects for major browsers */
